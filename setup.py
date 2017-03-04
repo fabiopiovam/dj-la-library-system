f6ccf1b9370358d30abb3ddd224134c2528a07f8
laborautonomo <fabio@laborautonomo.org>
2017-03-03 21:02:06 -0300

    [add] setup file

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+# allow setup.py to be run from any path and open files
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+try:
+    REQUIREMENTS = open('requirements.txt').read()
+except:
+    REQUIREMENTS = None
+
+
+try:
+    README = open('README.md').read()
+except:
+    README = None
+
+setup(
+    name='dj-la-library-system',
+    version="v0.1.0",
+    description=(
+        'Django Library System Application'
+    ),
+    long_description=README,
+    install_requires=REQUIREMENTS,
+    author='Fábio Piovam Elias',
+    author_email='fabio@laborautonomo.org',
+    url='https://github.com/laborautonomo/dj-la-library-system/',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
