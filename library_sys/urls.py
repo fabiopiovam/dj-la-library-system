adff3661c98fb80bfbaec83454f351a03be1febf
laborautonomo <fabio@laborautonomo.org>
2017-03-31 11:29:54 -0300

    [add] Frontend: Book details page

--- library_sys/urls.py
+++ library_sys/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     url(r'^logout/$', forms.LogoutView.as_view(), name='logout'),
     url(r'^change_pass/$', views.change_password, name='change_password'),
     url(r'^book_list/$', views.BookListView.as_view(), name='book_list'),
+    url(r'^(?P<slug>.*?)/$', views.book, name='book'),
 ]
